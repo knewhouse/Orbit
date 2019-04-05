81e802549363e81cf5a0441cce043f54d279044e
Nicole Guyton <nicoleguyton@Nicoles-Mac-mini.local>
2019-04-05 10:22:57 -0700

    update venues

--- js/theme-scripts.js
+++ js/theme-scripts.js
@@ -22,6 +22,35 @@ $(function() {
   });
 });
 
+// Initiate the wowjs animation library
+new WOW().init();
+
+// Initiate superfish on nav menu
+$('.nav-menu').superfish({
+  animation: {
+    opacity: 'show'
+  },
+  speed: 400
+});
+
+// Porfolio - uses the magnific popup jQuery plugin
+$('.portfolio-popup').magnificPopup({
+  type: 'image',
+  removalDelay: 300,
+  mainClass: 'mfp-fade',
+  gallery: {
+    enabled: true
+  },
+  zoom: {
+    enabled: true,
+    duration: 300,
+    easing: 'ease-in-out',
+    opener: function (openerElement) {
+      return openerElement.is('img') ? openerElement : openerElement.find('img');
+    }
+  }
+});
+
 // Owl carousel
 /* $('.owl-carousel').owlCarousel({
     loop:true,
