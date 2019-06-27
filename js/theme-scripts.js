e89659b61ed755ce91a82d70f3e99c0fad5a7dc9
Kari <newhousemusic@gmail.com>
2019-06-27 11:40:06 -0700

    added contact form functionality fixed path to team image

--- js/theme-scripts.js
+++ js/theme-scripts.js
@@ -21,8 +21,60 @@ $(function() {
     }
   });
 });
+// Contact form
+$("#contactForm").validator().on("submit", function (event) {
+    if (event.isDefaultPrevented()) {
+        // handle the invalid form...
+        formError();
+        submitMSG(false, "Did you fill in the form properly?");
+    } else {
+        // everything looks good!
+        event.preventDefault();
+        submitForm();
+    }
+});
 
 
+function submitForm(){
+    // Initiate Variables With Form Content
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+
+    $.ajax({
+        type: "POST",
+        url: "php/form-process.php",
+        data: "name=" + name + "&email=" + email + "&message=" + message,
+        success : function(text){
+            if (text == "success"){
+                formSuccess();
+            } else {
+                formError();
+                submitMSG(false,text);
+            }
+        }
+    });
+}
+
+function formSuccess(){
+    $("#contactForm")[0].reset();
+    submitMSG(true, "Thank you! Your message has been sent")
+}
+
+function formError(){
+    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+        $(this).removeClass();
+    });
+}
+
+function submitMSG(valid, msg){
+    if(valid){
+        var msgClasses = "h3 text-center tada animated text-success";
+    } else {
+        var msgClasses = "h3 text-center text-danger";
+    }
+    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+}
 
 // Owl carousel
 /* $('.owl-carousel').owlCarousel({
@@ -46,7 +98,7 @@ $(function() {
 }) */
 
 $('#myCarousel').carousel({
-  interval: 2800
+  interval: 5000
  })
 
   // hide #back-top first
